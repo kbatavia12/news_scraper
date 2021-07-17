f97c86862e7e22be9d585015b58bb99f20ee842e
Karan Batavia <42770054+kbatavia12@users.noreply.github.com>
2021-07-17 15:25:30 +0530

    cors fix

--- app.py
+++ app.py
@@ -1,24 +1,22 @@
 from flask import Flask, request, jsonify
 from scraper import get_news
-from flask_cors import CORS, cross_origin
+
 app = Flask(__name__)
-cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 
 @app.route('/')
-@cross_origin()
 def index():
     return "This is the index of the news api"
 
 @app.route('/get-news')
-@cross_origin()
 def send_news():
     news_number = int(request.args.get('number'))
     news = get_news()
-    return jsonify(news[5*(news_number-1): news_number*5])
+    response = jsonify(news[5*(news_number-1): news_number*5])
+    response.headers.add("Access-Control-Allow-Origin", "*");
+    return response
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-    
